2ff223a99c257eefa1e915e75d8efdd4fdef5c77
Sosso <gsohayb@gmail.com>
2023-06-10 17:28:59 +0200

    Slider works in the "next" direction
    * For now, works only if the number of existing items is >= than the amount of items that can be shown
    * Removed an example file from Vite

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 import './style.scss'
 
 let imageList = document.querySelector('.image-list');
-let firstEl = imageList.firstElementChild;
-imageList.appendChild(firstEl.cloneNode(true));
+if (needsClonedItem(imageList)) {
+  let firstEl = imageList.firstElementChild;
+  let clonedFirstEl = firstEl.cloneNode(true);
+  clonedFirstEl.classList.add('cloned');
+  imageList.appendChild(clonedFirstEl);
+}
 
-document.querySelector('body').addEventListener('click', function() {
+document.querySelector('.js-prev').addEventListener('click', () => moveSlide('prev'));
+document.querySelector('.js-next').addEventListener('click', () => moveSlide('next'));
+
+function moveSlide(direction = 'next') {
   let imageList = document.querySelector('.image-list');
-  if (imageList.classList.contains('move')) return;
+  let listNeedsClone = needsClonedItem(imageList);
+
+  if (imageList.classList.contains(`move-${direction}`)) return;
   
-  imageList.classList.toggle('move');
+  imageList.classList.add(`move-${direction}`);
+
   let firstEl = imageList.firstElementChild;
   let secondEl = imageList.children[1];
+  let elementToAppend = listNeedsClone ? secondEl : firstEl;
 
   setTimeout(() => {
-    imageList.classList.toggle('move');
+    imageList.classList.remove(`move-${direction}`);
     imageList.removeChild(firstEl);
-    imageList.appendChild(secondEl.cloneNode(true));
+    elementToAppend = elementToAppend.cloneNode(true);
+
+    if (listNeedsClone) {
+      imageList.querySelector('.cloned').classList.remove('cloned');
+      elementToAppend.classList.add('cloned');
+    }
+
+    imageList.appendChild(elementToAppend);
   }, 500);
-});
+  
+}
+
+function needsClonedItem(parent) {
+  let numberOfItemsToShow = getComputedStyle(parent).getPropertyValue('--_items-shown');
+  return getNumberOfOriginalItems(parent) == numberOfItemsToShow;
+}
+
+function getNumberOfOriginalItems(parent) {
+  return parent.querySelectorAll(':scope > :not(.cloned)').length;
+}
